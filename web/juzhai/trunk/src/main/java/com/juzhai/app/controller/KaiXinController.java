8833ef216afb9b77eed9fed9fb3dde23098e7579
xune987@gmail.com <xune987@gmail.com@fd904e1e-7d52-0d7d-8f42-489886cd414f>
2011-09-18 16:32:21 +0000

    [add:开心 feed request]

---
+++ web/juzhai/trunk/src/main/java/com/juzhai/app/controller/KaiXinController.java
@@ -0,0 +1,83 @@
+package com.juzhai.app.controller;
+
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.juzhai.app.util.AppPlatformUtils;
+import com.juzhai.core.controller.BaseController;
+import com.juzhai.core.exception.NeedLoginException;
+import com.juzhai.core.web.session.UserContext;
+import com.juzhai.passport.InitData;
+import com.juzhai.passport.bean.AuthInfo;
+
+@Controller
+public class KaiXinController  extends BaseController {
+	private final Log log = LogFactory.getLog(getClass());
+	
+	@RequestMapping(value = {"/kaixinFeed" }, method = RequestMethod.GET)
+	public String kaixinFeed(HttpServletRequest request,HttpServletResponse response, Model model,String name) throws NeedLoginException{
+		PrintWriter out=null;
+		UserContext context = checkLoginForApp(request);
+		try{
+			
+			AuthInfo authInfo =getAuthInfo(context.getUid(),context.getTpId());
+			authInfo.setAppSecret(InitData.TP_MAP.get(context.getTpId()).getAppSecret());
+		//拼凑参数
+		Map<String,String> paramMap=new HashMap<String,String>();
+		paramMap.put("method", "actions.sendNewsFeed");
+		paramMap.put("text", "我最近想去 【"+name+"】 ，有人响应么？");
+		paramMap.put("linktext", "去看看");
+		paramMap.put("link", "http://test.51juzhai.com");
+		String query=AppPlatformUtils.buildQuery(paramMap, authInfo.getAppKey(), authInfo.getAppSecret(), authInfo.getSessionKey(), "1.2");
+		out=response.getWriter();
+		out.println(AppPlatformUtils.urlBase64Encode(query));
+		}catch (Exception e) {
+			log.error("kaixin send feed is error", e);
+		}finally{
+			if(out!=null){
+				out.close();
+			}
+		}
+		return null;
+	}
+	
+	
+	@RequestMapping(value = {"/kaixinRequest" }, method = RequestMethod.GET)
+	public String kaixinRequest(HttpServletRequest request,HttpServletResponse response, Model model,String name) throws NeedLoginException{
+		PrintWriter out=null;
+		UserContext context = checkLoginForApp(request);
+		try{
+		AuthInfo authInfo =getAuthInfo(context.getUid(),context.getTpId());
+		authInfo.setAppSecret(InitData.TP_MAP.get(context.getTpId()).getAppSecret());
+		//拼凑参数
+		Map<String,String> paramMap=new HashMap<String,String>();
+		paramMap.put("method", "actions.sendInvitation");
+		paramMap.put("format", "json");
+		paramMap.put("mode", "0");
+		paramMap.put("text", "我最近想去 【"+name+"】 ，有人响应么？");
+		String query=AppPlatformUtils.buildQuery(paramMap, authInfo.getAppKey(), authInfo.getAppSecret(), authInfo.getSessionKey(), "1.2");
+		out=response.getWriter();
+		out.println(AppPlatformUtils.urlBase64Encode(query));
+		}catch (Exception e) {
+			log.error("kaixin send Request is error", e);
+		}finally{
+			if(out!=null){
+				out.close();
+			}
+		}
+		return null;
+	}
+	
+	
+}
